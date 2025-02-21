edf80d4be8f113d2fda87d593ebe04073db2dbdc
Bkevz <kevinamoni20@gmail.com>
2025-02-21 15:46:19 +0300

    Address updates

--- components/footer.tsx
+++ components/footer.tsx
@@ -26,7 +26,7 @@ export function Footer() {
               </div>
               <div className="flex items-center space-x-2 text-gray-400">
                 <MapPin className="h-4 w-4" />
-                <span>180 Travel Street, Upper Hill Nairobi</span>
+                <span>Eastleigh 12 Street 00610, Nairobi Kenya</span>
               </div>
             </div>
           </div>
